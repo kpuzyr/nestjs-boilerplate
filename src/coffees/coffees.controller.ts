f7943679c7bf72e86a0fe831e3d2d755faa6d394
nikolay.puzyr <nikolay.puzyr@onix-systems.com>
2020-08-13 18:02:11 +0300

    added e2e test

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -2,10 +2,10 @@ import { Controller, Get, Param, Post, Body, HttpCode, HttpStatus, Patch, Delete
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
-import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 import { Public } from '../common/decorators/public.decorator';
 import { ParseIntPipe } from '../common/pipes/parse-int.pipe';
-import { Protocol } from 'src/common/decorators/protocol.decorator';
+import { Protocol } from '../common/decorators/protocol.decorator';
 import { ApiResponse, ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
 
 
