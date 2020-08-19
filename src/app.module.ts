e1b6ad170afa37f3c754a78dc95b719a74eb4753
nikolay.puzyr <nikolay.puzyr@onix-systems.com>
2020-08-19 10:44:40 +0300

    added mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    MongooseModule.forRoot('mongodb://localhost:27020/nest-cource', { useFindAndModify: false })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CoffeeSchema, Coffee } from './entities/coffee.entity';
+import { Event, EventSchema } from '../events/entities/event.entity';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: Coffee.name,
+                schema: CoffeeSchema
+            },
+            {
+                name: Event.name,
+                schema: EventSchema
+            }
+        ])
+    ],
     controllers: [CoffeesController],
     providers: [CoffeesService]
 })
-export class CoffeesModule {}
+export class CoffeesModule { }
